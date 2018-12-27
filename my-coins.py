e14fd3808b6a346fb8c257eb86140a4efdd0894b
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-27 19:07:12 +0100

    Start main script from scratch.

--- my-coins.py
+++ my-coins.py
@@ -1,54 +1,8 @@
-import os, sys
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError
-from json import loads as jsonloads, dumps as jsondumps
-from constants import (BASE_URL, NAME, ALL, STATUS, BUYS, COINS, MARKET)
-from views import (print_status, print_buys, print_coins, print_market)
-from coinbase_views import (print_reduced_buys)
+from environment import Environment
+from coinbase_handler import CoinbaseHandler
 
-def _get_api_data(url):
-    _headers = {
-        'X-CMC_PRO_API_KEY': _my_data.get('CMC_API_KEY'),
-        'Accept': 'application/json'
-    }
-    _request = Request(url=url, headers=_headers)
-    _response = urlopen(_request)
-    return jsonloads(_response.read()).get('data')
-
-def _get_coinbase_data(url):
-    _headers = {
-        'CB-ACCESS-KEY': 'eCxVBKyfA4rJ21Bt',
-        'CB-ACCESS-SIGN': '',
-        'CB-ACCESS-TIMESTAMP': '',
-        'CONTENT-TYPE': 'application/json',
-    }
-
-_dir_path = os.path.dirname(os.path.realpath(__file__))
-_argv1 = sys.argv[1] if len(sys.argv) > 1 else None
-
-with open(_dir_path + '/' + NAME + '.json') as _file:
-    _my_data = jsonloads(_file.read())
-_coins = _my_data.get('coins')
-_fiat = _my_data.get('fiat')
-
-_query_params = {'symbol': ','.join(_coins.keys()), 'convert': _fiat}
-_query_string = '?' + '&'.join([_k + '=' + _v for _k, _v in _query_params.items()])
-_url =  BASE_URL + '/v1/cryptocurrency/quotes/latest' + _query_string
-_api_data = _get_api_data(_url)
-
-print(NAME, _fiat)
-
-if _argv1 in (ALL, MARKET):
-    _market_query_string = '?convert=' + _fiat
-    _market_api_url = BASE_URL + '/v1/global-metrics/quotes/latest?convert=' + _fiat
-    _market_api_data = _get_api_data(_market_api_url)
-    print_market(_my_data, _market_api_data)
-
-if not _argv1 or _argv1 in (ALL, STATUS):
-    print_status(_my_data, _api_data)
-
-if _argv1 in (ALL, COINS):
-    print_coins(_my_data, _api_data)
-
-if _argv1 in (ALL, BUYS):
-    print_reduced_buys(_my_data)
+environment = Environment()
+coinbaseHandler = CoinbaseHandler(
+    api_key=environment.cb_api_key,
+    api_secret=environment.cb_api_secret,
+)
